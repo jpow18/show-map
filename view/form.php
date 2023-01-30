160374460ecf2dfbfcbcb98daf397e22dfd16b5c
James Pow <jbpow18@icloud.com>
2023-01-30 18:15:15 -0500

    Added apiLogic.php to list.php and action attribute to form.

--- view/form.php
+++ view/form.php
@@ -1,6 +1,6 @@
 <form id="show-form" action="<?php $_SERVER['PHP_SELF'] ?>" method="GET">
   <label for="show-input">Enter Show Date (YYYY-MM-DD):</label>
-  <input type="text" id="show-input" pattern="\d{4}-\d{2}-\d{2}" title="Date must be in YYYY-MM-DD format" placeholder="2014-07-03">
+  <input type="text" name="show" id="show-input" pattern="\d{4}-\d{2}-\d{2}" title="Date must be in YYYY-MM-DD format" placeholder="2014-07-03">
   <button type="submit">Submit</button>
   <button type="reset">Reset</button>
 </form>
--- view/list.php
+++ view/list.php
@@ -1,7 +1,7 @@
 <section id="show-list">
   <h3>Your Shows:</h3>
   <ul>
-    <!-- Show entries will be added here -->
+    <?php include("./php/listLogic.php") ?>
   </ul>
 </section>
 <button id="map-shows">Map Your Shows</button>
