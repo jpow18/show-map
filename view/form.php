f85e1fc5364590b67029ac0c6f7bc556e9408257
James Pow <jbpow18@icloud.com>
2023-01-30 22:17:21 -0500

    Added onclick to reset button.

--- view/form.php
+++ view/form.php
@@ -2,5 +2,5 @@
   <label for="show-input">Enter Show Date (YYYY-MM-DD):</label>
   <input type="text" name="show" id="show-input" pattern="\d{4}-\d{2}-\d{2}" title="Date must be in YYYY-MM-DD format" placeholder="2014-07-03">
   <button type="submit">Submit</button>
-  <button type="reset">Reset</button>
+  <button type="reset" onclick="window.location.href='<?php echo $_SERVER['PHP_SELF']; ?>'">Reset</button>
 </form>
