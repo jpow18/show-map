85d75d67cb1a92eb8832c00bc9f0a92ffe67a312
James Pow <jbpow18@icloud.com>
2023-02-10 20:19:04 -0500

    feat: index.php format

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>Map Your Shows</title>
+</head>
+
+<body>
+  <header>
+    <h1>Map Your Shows</h1>
+    <nav>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+  </header>
+  <main>
+    <div class="form-container">
+      <form class="left-form">
+        <label for="shows-input">Enter the shows you've been to:</label>
+        <input type="text" id="shows-input" name="shows-input">
+        <input type="submit" value="Submit">
+      </form>
+      <form class="right-form">
+        <label for="map-shows-btn">Map Your Shows!</label>
+        <input type="submit" id="map-shows-btn" value="Submit">
+      </form>
+    </div>
+  </main>
+  <footer>
+    <p>Copyright &copy; 2023 Map Your Shows</p>
+    <p>Contact us at: <a href="mailto:contact@mapyourshows.com">contact@mapyourshows.com</a></p>
+  </footer>
+</body>
+
+</html>
